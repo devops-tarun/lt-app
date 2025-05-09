9ecf2ee1d3fdf06cfb6a97a21bb9e92201d72e6e
Tarun Sharma <37244849+devops-tarun@users.noreply.github.com>
2025-05-09 23:48:31 +0530

    Create variables.tf

---
+++ mern-tf-infra/modules/ec2/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {}
+variable "private_subnet_id" {}
+variable "key_name" {}
+variable "ami_id" {}
+variable "instance_type" {}
+variable "user_data_path" {}
+variable "alb_sg_id" {}
