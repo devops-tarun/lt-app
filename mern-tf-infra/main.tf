ff808e4ccf043466c7ca1a6a2311cb8331dac858
Tarun Sharma <37244849+devops-tarun@users.noreply.github.com>
2025-05-09 23:45:27 +0530

    Create main.tf

---
+++ mern-tf-infra/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source               = "./modules/vpc"
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnet_cidr   = "10.0.1.0/24"
+  private_subnet_cidr  = "10.0.2.0/24"
+  az                   = "us-east-1a"
+}
+
+module "ec2" {
+  source             = "./modules/ec2"
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_id  = module.vpc.private_subnet_id
+  key_name           = var.key_name
+  ami_id             = "ami-0c55b159cbfafe1f0"
+  instance_type      = "t3.medium"
+  user_data_path     = "${path.module}/userdata.sh"
+  alb_sg_id          = module.alb.alb_sg_id
+}
+
+module "alb" {
+  source            = "./modules/alb"
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_id  = module.vpc.public_subnet_id
+  instance_id       = module.ec2.instance_id
+}
