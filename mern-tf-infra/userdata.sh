f7385b96b108a5fe92cc5199c66a815335ce983e
Tarun Sharma <37244849+devops-tarun@users.noreply.github.com>
2025-05-10 13:52:23 +0530

    Update userdata.sh

--- mern-tf-infra/userdata.sh
+++ mern-tf-infra/userdata.sh
@@ -14,8 +14,8 @@ node -v
 npm -v
 
 # Install MongoDB
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
 apt-get update
 apt-get install -y mongodb-org
 
